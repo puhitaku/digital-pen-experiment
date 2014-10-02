2494d2ea4e1c7d5cfa51972f22137d023e7f762a
puhitaku <puhitaku@gmail.com>
2014-10-02 16:58:44 +0900

    added some funcs

--- Algebra.py
+++ Algebra.py
@@ -1,4 +1,5 @@
 from numpy import matrix
+from numpy.linalg import norm
 from math import atan2, sin, cos
 
 def angle_from_vector(vec):
@@ -15,5 +16,41 @@ def get_rot_matrix(t):
 def shift_position(a, b):
     return tuple(sum(x) for x in zip(a, b))
 
-def vec_between_two_pts(a, b):
-    return matrix()
+def vec_between_two(a, b, normalize=False, rtype=matrix):
+    """Calculates the vector between a and b.
+
+    Args:
+      a (np.matrix): Point A.
+      b (np.matrix): Point B.
+      normalize (bool): Toggle normalization.
+      rtype (type): The type of returned value.
+
+    Returns:
+      Vector between A and B. It'll be normalized if needed.
+
+    """
+
+    if type(a) == tuple or type(a) == list:
+        _a, _b = matrix(a), matrix(b)
+    elif type(a) == matrix:
+        _a, _b = a, b
+        
+    vec = _b - _a
+    n = norm(vec)
+    if n > 0:
+        vec_normalized = vec / n
+    else:
+        vec_normalized = vec
+
+    if rtype == matrix:
+        return vec_normalized if normalize else vec
+    elif rtype == tuple or rtype == list:
+        if normalize:
+            t = vec_normalized.tolist()
+        else:
+            t = vec.tolist()
+        return (t[0][0], t[0][1]) if rtype == tuple else [t[0][0], t[0][1]]
+
+def get_stroke_distance(st):
+    relatives = [vec_between_two(v[0], v[1], rtype=matrix) for v in zip(st[:-1], st[1:])]
+    return sum( norm(v) for v in relatives )
