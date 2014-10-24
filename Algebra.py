a4eb3917dfc758f63543bbb031174c7c1cf4d946
puhitaku <puhitaku@gmail.com>
2014-10-24 22:13:55 +0900

    Added misc funcs

--- Algebra.py
+++ Algebra.py
@@ -2,6 +2,14 @@ from numpy import matrix
 from numpy.linalg import norm
 from math import atan2, sin, cos
 
+def rvec_to_tuple(mat):
+    l = mat.tolist()
+    return (l[0][0], l[1][0])
+
+def lvec_to_tuple(mat):
+    l = mat.tolist()
+    return (l[0][0], l[0][1])
+
 def angle_from_vector(vec):
     return atan2(vec.tolist()[1][0], vec.tolist()[0][0])
 
@@ -54,3 +62,6 @@ def vec_between_two(a, b, normalize=False, rtype=matrix):
 def get_stroke_distance(st):
     relatives = [vec_between_two(v[0], v[1], rtype=matrix) for v in zip(st[:-1], st[1:])]
     return sum( norm(v) for v in relatives )
+
+def middle_between_two(a, b):
+    return ( (a[0]+b[0])/2, (a[1]+b[1])/2 )
