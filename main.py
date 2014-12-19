b1f65f9894c5c10e6cd7df82671b112002071047
puhitaku <puhitaku@gmail.com>
2014-12-19 15:32:05 +0900

    Added new main code

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Digital Pen Experiment (Educator Support Tool w/ Digital Pen)
+
+"""
+
+import sys
+
+def main():
+    pass
+
+if __main__ == '__main__':
+    main()
+    sys.exit()
