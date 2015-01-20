601a21ccc511bc24c5199b5505915e20724cfc7c
puhitaku <puhitaku@gmail.com>
2015-01-20 14:40:36 +0900

    Construct explicitly

--- plugins/parser/SerialParser.py
+++ plugins/parser/SerialParser.py
@@ -1,5 +1,8 @@
 import BasePlugin
 
 class SerialParser(BasePlugin.ParserPlugin):
+    def __init__(self):
+        super().__init__()
+
     def invoke(self):
         self.log.debug('Successfully loaded :)')
